8dcd237063ed5639aa16f0a85e62f09d9a69bcd1
Mr-Personne <h.sacha07@gmail.com>
2020-01-06 19:30:42 +0000

    mise en place systeme isset  else afficher index-view.php

--- controller/controller.php
+++ controller/controller.php
@@ -1,36 +1,45 @@
 <?php
 
-    //va chercher infos dans database sur film selectionné
-    require __DIR__ . '/../model/movies.php';
-    
-    
-    //vérifie si le fichier pour l'image de l'affiche est existe bien en jpg ou png
-    //si oui, met le lien vers l'image dans $lienAffiche et va chercher sa taille en largeur et hauteur
-    if (file_exists("public/images/trailer-".$allData[0]['id'].".png")){
-        $lienAffiche = "public/images/trailer-".$allData[0]['id'].".png";
-        $imgSize = getimagesize("public/images/trailer-".$allData[0]['id'].".png");
-        $imgWidth = $imgSize[0];
-        $imgHeight = $imgSize[1];
-        // echo "<p>lien affiche : " . $lienAffiche . "; imgW : " . $imgWidth . "; imgh : " . $imgHeight ."</p>";
-        clearstatcache();
-    }
-    else if (file_exists("public/images/trailer-".$allData[0]['id'].".jpg")){
-        $lienAffiche = "public/images/trailer-".$allData[0]['id'].".jpg";
-        $imgSize = getimagesize("public/images/trailer-".$allData[0]['id'].".jpg");
-        $imgWidth = $imgSize[0];
-        $imgHeight = $imgSize[1];
-        // echo "<p>lien affiche : " . $lienAffiche . "; imgW : " . $imgWidth . "; imgh : " . $imgHeight ."</p>";
-        clearstatcache();
+    if(isset($_GET['filmId'])){
+        //va chercher infos dans database sur film selectionné
+        require __DIR__ . '/../model/movies.php';
+        
+        
+        //vérifie si le fichier pour l'image de l'affiche est existe bien en jpg ou png
+        //si oui, met le lien vers l'image dans $lienAffiche et va chercher sa taille en largeur et hauteur
+        if (file_exists("public/images/trailer-".$allData[0]['id'].".png")){
+            $lienAffiche = "public/images/trailer-".$allData[0]['id'].".png";
+            $imgSize = getimagesize("public/images/trailer-".$allData[0]['id'].".png");
+            $imgWidth = $imgSize[0];
+            $imgHeight = $imgSize[1];
+            // echo "<p>lien affiche : " . $lienAffiche . "; imgW : " . $imgWidth . "; imgh : " . $imgHeight ."</p>";
+            clearstatcache();
+        }
+        else if (file_exists("public/images/trailer-".$allData[0]['id'].".jpg")){
+            $lienAffiche = "public/images/trailer-".$allData[0]['id'].".jpg";
+            $imgSize = getimagesize("public/images/trailer-".$allData[0]['id'].".jpg");
+            $imgWidth = $imgSize[0];
+            $imgHeight = $imgSize[1];
+            // echo "<p>lien affiche : " . $lienAffiche . "; imgW : " . $imgWidth . "; imgh : " . $imgHeight ."</p>";
+            clearstatcache();
+        }
+        else{
+            $lienAffiche = "";
+            $imgWidth = "";
+            $imgHeight = "";
+            // echo "<p>erreur, fichier inexistant ou pas en format jpg ou png</p>";
+        }
+
+        //ouvre et affiche la page du film avec les infos recupérées
+        require __DIR__ . '/../view/film-info.php';
     }
     else{
-        $lienAffiche = "";
-        $imgWidth = "";
-        $imgHeight = "";
-        // echo "<p>erreur, fichier inexistant ou pas en format jpg ou png</p>";
+        //va chercher infos dans database sur tous les films
+        require __DIR__ . '/../model/movies.php';
+        require __DIR__ . '/../view/index-view.php';
+        print_r($allData);
     }
-
-    //ouvre et affiche la page du film avec les infos recupérées
-    require __DIR__ . '/../view/film-info.php';
+    
 
     
 ?>
