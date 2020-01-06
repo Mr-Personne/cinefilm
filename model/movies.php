1294f5c55d622ce3a2b31c458cd2e010e7fc6dff
Mr-Personne <h.sacha07@gmail.com>
2020-01-06 22:02:28 +0000

    systeme verif genre selectioné et ajout requete sql adaptée

--- model/movies.php
+++ model/movies.php
@@ -1,19 +1,34 @@
 <!-- Création du models movies.php qui contiendrait toutes fonctions des requêtes sur la table films -->
 <?php
     // print_r($_POST);
+    //verif si la page demandé est un film specifique avec un id specifique
     if(isset($_GET['filmId'])){
         $search = $_GET['filmId'];
     }
-    else{
-        $search = "index";
+    //sinon, verif si la demande est d'afficher des films par genre
+    elseif(isset($_GET['genre'])){
+
+        if($_GET['genre'] == "tous"){
+            $search = "tous";
+        }
+        else{
+            $search = "genre";
+            $nomGenre = $_GET['genre'];
+        }
+        
     }
+    //sinon, par default, afficher tous les films
+    else{
+        $search = "tous";
+    }
+    
     
 
     try{
         $db = new PDO('mysql:host=localhost;dbname=dbFilms;charset=utf8', 'root', '1234');
         
         
-        if($search == "index"){
+        if($search == "tous"){
             //------méthode plus sécurisé contre injection SQL------
             $answer = $db->prepare('SELECT * FROM `films`
             INNER JOIN films_has_genre ON films_has_genre.films_id = films.id
@@ -24,6 +39,18 @@
             ORDER BY `films`.`id` ASC');
             
             
+        }
+        elseif($search == "genre"){
+            $answer = $db->prepare('SELECT * FROM `films`
+            INNER JOIN films_has_genre ON films_has_genre.films_id = films.id
+            INNER JOIN genre ON genre.idgenre = films_has_genre.genre_idgenre
+            INNER JOIN films_has_realisateur ON films_has_realisateur.films_id = films.id
+            INNER JOIN realisateur ON realisateur.idrealisateur = films_has_realisateur.realisateur_idrealisateur
+            WHERE nomGenre = :filmGenre
+            GROUP BY titre');
+
+            $filmGenre = $nomGenre;
+            $answer->bindParam(':filmGenre', $filmGenre, PDO::PARAM_STR);
         }
         else{
             //------méthode plus sécurisé contre injection SQL------
